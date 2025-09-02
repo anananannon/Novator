00bc13041822722a0c35401ef86053c4c0561483
anananannon <dzgoev10108@icloud.com>
2025-09-02 05:06:50 -0700

    aa few change

--- Novator/Views/Study/StudyView.swift
+++ Novator/Views/Study/StudyView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+
 // MARK: - StudyView
 struct StudyView: View {
     // MARK: - Observed & State Objects
@@ -6,53 +7,66 @@ struct StudyView: View {
     @State private var navigationPath = NavigationPath()
     @Binding var selectedTab: Int
     @State private var showPopover = false
-    
     @State private var selectedLesson: Lesson? = nil
-
+    
+    @State private var isTapButton = false
+    
     
     // MARK: - Initialization
-    init(profile: UserProfileViewModel,
-         selectedTab: Binding<Int>) {
+    init(profile: UserProfileViewModel, selectedTab: Binding<Int>) {
         self.profile = profile
         self._selectedTab = selectedTab
-    };
+    }
+
     // MARK: - Body
     var body: some View {
         NavigationStack(path: $navigationPath) {
             GeometryReader { geometry in
                 ScrollView {
-                    VStack(spacing: 20) {
+                    LazyVStack(spacing: 20) { // <-- LazyVStack вместо VStack
                         ForEach(TaskManager.lessons.reversed().chunked(into: 10), id: \.self) { chunk in
-                            VStack(spacing: 20) {
-                                ForEach(chunk, id: \.id) { lesson in
-                                    Button(action: {
-                                        selectedLesson = lesson
-                                    }, label: {
-                                        RoundedRectangle(cornerRadius: 20)
-                                            .fill(profile.isLessonCompleted(lesson.id) ? Color("AppRed") : Color(.gray))
-                                            .frame(minWidth: 30, minHeight: 30)
-                                            .frame(maxWidth: 70, maxHeight: 70)
-                                            .aspectRatio(1, contentMode: .fit)
-                                            .overlay {
-                                                Text("\(lesson.id)")
-                                                    .font(.headline)
-                                                    .foregroundColor(profile.isLessonCompleted(lesson.id) ? Color(.white) : .primary)
+                            LazyVStack(spacing: 20) { // вложенный LazyVStack
+                                ForEach(chunk.indices, id: \.self) { index in
+                                    let lesson = chunk[index]
+                                    HStack {
+                                        if index % 2 == 0 {
+                                            ZStack(alignment: .leading) {
+                                                RoundedRectangle(cornerRadius: 23).fill(Color("TaskBackground"))
+                                                    .frame(height: 76)
+                                                    .frame(minWidth: 200)
+                                                    .opacity(isTapButton ? 1:0)
+                                                    .scaleEffect(x: isTapButton ? 1 : 0, anchor: .leading)
+                                                
+                                                lessonButton(lesson)
                                             }
-                                    })
+                                            Spacer()
+                                        } else {
+                                            Spacer()
+                                            ZStack(alignment: .trailing) {
+                                                RoundedRectangle(cornerRadius: 23).fill(Color("TaskBackground"))
+                                                    .frame(height: 76)
+                                                    .frame(minWidth: 200)
+                                                    .opacity(isTapButton ? 1:0)
+                                                    .scaleEffect(x: isTapButton ? 1 : 0, anchor: .trailing)
+                                                lessonButton(lesson)
+                                            }
+                                        }
+                                    }
                                 }
                             }
-                            
+
                             if chunk != TaskManager.lessons.reversed().chunked(into: 10).last {
                                 Divider()
-                                    .frame(minHeight: 1.5)
-                                    .overlay(Color("AppRed"))
+                                    .frame(height: 1.5)
+                                    .background(Color("AppRed"))
                                     .padding(.horizontal, 10)
                             }
                         }
-
                     }
-                    .frame(maxWidth: .infinity, minHeight: geometry.size.height) // Устанавливаем минимальную высоту
+                    .animation(.linear(duration: 0.2), value: isTapButton)
+                    .frame(maxWidth: .infinity, minHeight: geometry.size.height)
                     .padding()
+                    .padding(.horizontal, 40    )
                     .navigationBarTitleDisplayMode(.inline)
                     .toolbar { toolbarContent }
                 }
@@ -64,9 +78,9 @@ struct StudyView: View {
         .preferredColorScheme(profile.theme.colorScheme)
     }
 }
+
 // MARK: - Subviews & ViewBuilders
 private extension StudyView {
-    // MARK: - Toolbar Content
     var toolbarContent: some ToolbarContent {
         ToolbarItem(placement: .topBarLeading) {
             Button {
@@ -84,14 +98,36 @@ private extension StudyView {
             .preferredColorScheme(profile.theme.colorScheme)
         }
     }
+
+    @ViewBuilder
+    func lessonButton(_ lesson: Lesson) -> some View {
+        ZStack(alignment: .leading) {
+            Button(action: {
+                if isTapButton {
+                    selectedLesson = lesson
+                } else {
+                    isTapButton.toggle()
+                }
+            }) {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(profile.isLessonCompleted(lesson.id) ? Color("AppRed") : Color(.gray))
+                    .frame(width: 70, height: 70)
+                    .overlay {
+                        Text("\(lesson.id)")
+                            .font(.headline)
+                            .foregroundColor(profile.isLessonCompleted(lesson.id) ? .white : .primary)
+                    }
+            }
+            .buttonStyle(.plain)
+            .padding(.horizontal, 3)
+        }
+    }
 }
+
 // MARK: - Preview
 struct StudyView_Previews: PreviewProvider {
     static var previews: some View {
-        StudyView(
-            profile: UserProfileViewModel(),
-            selectedTab: .constant(0)
-        )
+        StudyView(profile: UserProfileViewModel(), selectedTab: .constant(0))
     }
 }
 
